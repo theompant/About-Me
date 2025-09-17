3b75b48b7b8e3696f586df266ecdecb3cb3d3a7a
theompant <ompant624@gmail.com>
2025-09-17 18:17:14 +0530

    changed

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: { unoptimized: true },
-  experimental: { typedRoutes: true }
-};
-module.exports = nextConfig;
