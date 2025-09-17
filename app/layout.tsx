03a5bb2433b4b9e3847a8d933b4174011788982d
theompant <ompant624@gmail.com>
2025-09-17 18:14:16 +0530

    Revert "Update layout.tsx"
    This reverts commit 96de2e9a87432206998048ee493eceee105a3a7a.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,5 @@
 // app/layout.tsx
 import "../styles/globals.css";
-import "../styles/tokens.css"; // new
-import "../styles/glass.css";  // new
-import "../styles/card-typography.css"; // new
-
  // adjust path if needed
 import { ThemeProvider } from "next-themes";
 import type { Metadata } from "next";
